519ec45eff40472c1583b98fbb5c55574f2da320
Manas Pradhan <manasmpradhan5@gmail.com>
2022-01-29 10:27:16 +0530

    created operator function

---
+++ script.js
@@ -0,0 +1,15 @@
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return 'ERROR';
+    }
+}
+
