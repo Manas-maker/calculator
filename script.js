fc5dbd60314cd55bc615de8a71b3e475a09df2d8
Manas Pradhan <manasmpradhan5@gmail.com>
2022-01-31 00:40:10 +0530

    can solve two term operations

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const display = document.querySelector('#display');
+let num1;
+let num2;
+let opp;
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+let stat = 1;
+let dec = 0;
+
+digits.forEach((digit) => digit.addEventListener('click', takeInputNum));
+operators.forEach((operator) => operator.addEventListener('click', takeInputOperator));
+
+function takeInputNum(aPointer) {
+    a = aPointer.target.value;
+    if (a != '0' || dec == 0) {
+    display.textContent += a;
+    if (a == '.') dec=1;
+    }
+    if (stat == 1) num1 += a;
+    else num2 += a;
+}
+
+function takeInputOperator(bPointer) {
+    b = bPointer.target.textContent;
+    if (b=='C') {
+        num1 = '';
+        num2 = '';
+        stat = 1;
+        dec = 0;
+        display.textContent = '';
+    }
+    else if (!isNaN(Number(num1))) {
+        if (b=='=') {
+            display.textContent = operate(opp, Number(num1), Number(num2));
+            num1 = display.textContent;
+        }
+        else {
+            display.textContent = '';
+            opp = b;
+            stat = 2;
+        }
+    }
+}
 function operate(operator, a, b) {
     switch(operator) {
         case '+':
