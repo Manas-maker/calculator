785a6dd7762fd660d67e7de66f9afc6ec407363e
Manas Pradhan <manasmpradhan5@gmail.com>
2022-02-03 00:26:01 +0530

    added decimal point support

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const display = document.querySelector('#display');
-let num1;
-let num2;
+let num1 = '';
+let num2 = '';
 let opp;
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 let stat = 1;
-let dec = 0;
+let dec1 = 0;
+let dec2 = 0;
 
 digits.forEach((digit) => digit.addEventListener('click', takeInputNum));
 operators.forEach((operator) => operator.addEventListener('click', takeInputOperator));
 
-function takeInputNum(aPointer) {
-    a = aPointer.target.value;
-    if (a != '0' || dec == 0) {
-    display.textContent += a;
-    if (a == '.') dec=1;
+function takeInputNum(numPointer) {
+    num = numPointer.target.value;
+    if (num != '.' || (stat == 1 && dec1 == 0) ||(stat == 2 && dec2 == 0)) {
+    display.textContent += num;
+    if (num == '.') {
+        switch(stat) {
+            case 1:
+                dec1 = 1;
+                break;
+            case 2:
+                dec2 = 1;
+                break;
+        }
+    };
     }
-    if (stat == 1) num1 += a;
-    else num2 += a;
+    if (stat == 1) num1 += num;
+    else num2 += num;
 }
 
 function takeInputOperator(bPointer) {
@@ -26,23 +36,25 @@ function takeInputOperator(bPointer) {
         num1 = '';
         num2 = '';
         stat = 1;
-        dec = 0;
+        dec1 = 0;
+        dec2 = 0;
+        let opp;
         display.textContent = '';
     }
-    else if (!isNaN(Number(num1))) {
-        if (b=='=') {
+    else if (num1) {
+        if (b=='=' && num2) {
             display.textContent = operate(opp, Number(num1), Number(num2));
             num1 = display.textContent;
             num2='';
             stat = 2;
         }
-        else if (stat==2) {
+        else if (stat==2 && num2) {
             display.textContent = '';
             num1 = operate(opp, Number(num1), Number(num2));
             num2='';
             stat = 2;
         }
-        else {
+        else if (b!='=') {
             display.textContent = '';
             opp = b;
             stat = 2;
@@ -50,19 +62,29 @@ function takeInputOperator(bPointer) {
     }
 }
 function operate(operator, a, b) {
+    let result;
     switch(operator) {
         case '+':
-            return a + b;
+            result =  a + b;
+            break;
         case '-':
-            return a - b;
+            result =  a - b;
+            break;
         case '*':
         case 'x':
         case 'X':
-            return a * b;
+            result =  a * b;
+            break;
         case '/':
-            return a / b;
+            result =  (b==0)?'No':a/b;
+            break;
         default:
-            return 'ERROR';
+            result =  a;
+            
     }
+    if (String(result).includes('.')) {
+        return (Math.round(result*100)/100)
+    }
+    return result;
 }
 
